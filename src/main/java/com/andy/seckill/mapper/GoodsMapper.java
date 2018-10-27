805e41ed97e45f3771d00586328fe88978f540fc
leone <exklin@gmail.com>
2018-10-27 09:45:55 +0800

    feat(all):update

--- src/main/java/com/andy/seckill/mapper/GoodsMapper.java
+++ src/main/java/com/andy/seckill/mapper/GoodsMapper.java
@@ -15,7 +15,7 @@ import java.util.List;
 public interface GoodsMapper {
 	
 	@Select("select * from t_goods where status = 0")
-    //@Results(value={@Result(column="a",property="b")})配置和列名和不同的属性名
+    //@Results({@Result(id=true,column="a",property="b")})配置和列名和不同的属性名
 	List<GoodsVO> list();
 
 	@Select("select * from t_goods where status = 0 and goods_id = #{goodsId}")
