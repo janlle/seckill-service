f3a7335300b4873afc566427e51ea281542a0179
leone <janlle@126.com>
2018-11-05 11:40:58 +0800

    feat(all):add goods detail

--- src/main/java/com/andy/seckill/mapper/GoodsMapper.java
+++ src/main/java/com/andy/seckill/mapper/GoodsMapper.java
@@ -1,5 +1,6 @@
 package com.andy.seckill.mapper;
 
+import com.andy.seckill.vo.GoodsDetailVO;
 import com.andy.seckill.vo.GoodsVO;
 import org.apache.ibatis.annotations.*;
 
@@ -19,7 +20,7 @@ public interface GoodsMapper {
 	List<GoodsVO> list();
 
 	@Select("select * from t_goods where status = 0 and goods_id = #{goodsId}")
-	GoodsVO findById(@Param("goodsId") Long goodsId);
+    GoodsDetailVO findById(@Param("goodsId") Long goodsId);
 
 	@Update("update t_goods set stock_count = stock_count - 1 where goods_id = #{goodsId} and stock_count > 0")
 	int reduceStock(@Param("goodsId") Long goodsId);
