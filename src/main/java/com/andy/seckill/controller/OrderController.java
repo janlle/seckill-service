f942b2adf104c3139f53c1556dd06f84633e97f7
leone <exklin@gmail.com>
2018-11-05 12:52:03 +0800

    feat(all):update

--- src/main/java/com/andy/seckill/controller/OrderController.java
+++ src/main/java/com/andy/seckill/controller/OrderController.java
@@ -1,29 +1,30 @@
 package com.andy.seckill.controller;
 
 import com.andy.seckill.common.Response;
-import com.andy.seckill.domain.Order;
-import com.andy.seckill.service.GoodsService;
 import com.andy.seckill.service.OrderService;
 import com.andy.seckill.service.UserService;
-import com.andy.seckill.vo.GoodsVO;
-import com.andy.seckill.vo.OrderVO;
+import com.andy.seckill.vo.OrderDetailVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
-//	@Autowired
-//    private UserService userService;
-//
-//	@Autowired
-//	private OrderService orderService;
-//
-//	@Autowired
-//    private GoodsService goodsService;
-//
+    private UserService userService;
+
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(UserService userService, OrderService orderService) {
+        this.userService = userService;
+        this.orderService = orderService;
+    }
+
 //    @GetMapping("/detail")
 //    public Response<GoodsVO> info(Model model, @RequestParam("orderId") long orderId) {
 //    	Order order = orderService.getOrderById(orderId);
@@ -34,5 +35,11 @@ public class OrderController {
 //    	GoodsVO goodsVO = goodsService.getGoodsVoByGoodsId(goodsId);
 //    	return Response.success(goodsVO);
 //    }
-    
+
+    @GetMapping("/detail")
+    public Response detail(Model model, @RequestParam("orderId") Long orderId) {
+        OrderDetailVO order = orderService.findOne(orderId);
+        return Response.success(null);
+    }
+
 }
