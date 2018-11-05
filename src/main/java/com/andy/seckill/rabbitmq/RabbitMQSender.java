1fe181cc71a3210f4a21905cc7acd98db2ef1a92
leone <janlle@126.com>
2018-11-05 15:32:35 +0800

    feat(all):add rabbitmq config

---
+++ src/main/java/com/andy/seckill/rabbitmq/RabbitMQSender.java
@@ -0,0 +1,13 @@
+package com.andy.seckill.rabbitmq;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ *
+ * @author Leone
+ * @since 2018-11-05
+ **/
+@Component
+public class RabbitMQSender {
+}
