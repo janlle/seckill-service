933af3379fa7a4b5f4553875814ddd7007a9bd72
leone <janlle@126.com>
2018-11-06 15:55:00 +0800

    feat(all):update

--- src/main/java/com/andy/seckill/domain/OrderDetail.java
+++ src/main/java/com/andy/seckill/domain/OrderDetail.java
@@ -1,5 +1,7 @@
 package com.andy.seckill.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,6 +11,7 @@ import java.util.Date;
  * @author Leone
  * @since 2018-10-26
  **/
+@Data
 public class OrderDetail implements Serializable {
 
     private Long orderDetailId;
@@ -27,77 +30,4 @@ public class OrderDetail implements Serializable {
 
     private Date createTime;
 
-    private Date updateTime;
-
-    public Long getOrderDetailId() {
-        return orderDetailId;
-    }
-
-    public void setOrderDetailId(Long orderDetailId) {
-        this.orderDetailId = orderDetailId;
-    }
-
-    public Long getGoodsId() {
-        return goodsId;
-    }
-
-    public void setGoodsId(Long goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Integer getTotal() {
-        return total;
-    }
-
-    public void setTotal(Integer total) {
-        this.total = total;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
--- src/main/java/com/andy/seckill/exception/GlobalExceptionHandler.java
+++ src/main/java/com/andy/seckill/exception/GlobalExceptionHandler.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @author Leone
  **/
+@ResponseBody
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
